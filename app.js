4718b79d4829b6a6d41754066db343f17ae28b07
martinasartori97 <martinasartori9@gmail.com>
2024-11-04 22:20:37 +0100

    edit js

--- app.js
+++ app.js
@@ -8,16 +8,13 @@ const app = express()
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
-app.use(express.static('/public'))
+app.use(express.static('public'));
 
-app.listen(3000, (req, res) => {
-    console.log(`server is running at  "http://127.0.0.1":3000}`);
-})
 
 
 app.get('/'(req, res) => {
     res.send('posts rest API')
-})
+});
 
 const posts = require('./db/posts.js')
 app.get('/posts', (req, res) => {
@@ -29,15 +26,11 @@ app.get('/posts', postsController.index);
 
 
 
+app.get('/posts', PostsController.index);
 app.get('/posts/:id', PostsController.show);
 
 
-const express = require('express')
-const app = express()
-const PizzaController = require('./controllers/PizzaController');
-// static files (optional)
-app.use(express.static('/public'))
 
-app.get('/pizze', PizzaController.index);
-app.get('/pizze/:id', PizzaController.show);
-
+app.listen(3000, () => {
+    console.log(`server is running at http://127.0.0.1:3000}`)
+  });
