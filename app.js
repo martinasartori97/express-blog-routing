a15d0b76802ecd7c3a321c5807d3e6b25d30e099
martinasartori97 <martinasartori9@gmail.com>
2024-11-04 15:37:16 +0100

    edit js

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ const HOST = process.env.HOST
 const PORT = process.env.PORT
 
 app.listen(PORT, (req,res) => {
-    console.log(`server is running at ${host}: ${port}`);
+    console.log(`server is running at ${HOST}: ${PORT}`);
   })
+
+
+app.get(','(req,res) => {
+    res.send('posts rest API')
+})
+
+// POSTS API
+app.get('/pizze',(req,res) => {
+    res.json({
+        data:
+        counter:
+    })
+})
+
 
